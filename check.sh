b73168112a45ff99896d264b91a5d18a587f6784
Klipspringer <klipspringr@users.noreply.github.com>
2025-07-21 11:24:37 +0100

    check.sh: silence git push

--- check.sh
+++ check.sh
@@ -5,12 +5,12 @@ cd "$(dirname "$0")"
 # shellcheck disable=1091
 . .env/bin/activate
 
-git pull
+git pull >/dev/null 2>&1
 
 python ./check.py
 
 git add blog/content/posts
 if ! git diff-index --quiet HEAD; then
     git -c "user.name=mefi-activity-automated" -c "user.email=mefi-activity-automated" commit -m "Recent mod actions updated"
-    git push
+    git push >/dev/null 2>&1
 fi
