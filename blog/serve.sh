9c23182b1d02d0817b82755fcda84bf0145ff45e
Klipspringer <97002039+klipspringr@users.noreply.github.com>
2026-04-12 12:18:05 +0100

    use this newfangled uv thing. and just use npx for pagefind

--- blog/serve.sh
+++ blog/serve.sh
@@ -5,5 +5,5 @@ hugo mod verify
 hugo list published > assets/data/published-content.csv
 rm -rf public
 hugo build
-.env/bin/python3 -m pagefind --site public
+npx -y pagefind@v1.5.0 --site public
 hugo server --bind=0.0.0.0
--- check/check.sh
+++ check/check.sh
@@ -3,9 +3,6 @@ set -euo pipefail
 
 cd "$(dirname "$0")"
 
-# shellcheck disable=1091
-. .env/bin/activate
-
 if [ -n "$(git status --porcelain)" ]; then
     echo "Uncommitted changes in working tree, exiting" >&2
     exit 1
@@ -13,11 +10,11 @@ fi
 
 git pull --quiet
 
-python ./check.py
+uv run ./check.py
 
-git add ../blog/content/posts
-
-if ! git diff-index --quiet HEAD; then
+if [ -n "$(git status --porcelain ../blog/content/posts)" ]; then
+    git add ../blog/content/posts
+    
     git -c "user.name=mefi-activity-automated" -c "user.email=mefi-activity-automated" commit -m "Recent mod actions updated"
     git push --quiet
 else
