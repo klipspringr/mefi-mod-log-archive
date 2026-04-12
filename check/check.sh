5f4b0dd26de150ee8cb4b94fceb8ac8b2b3754f7
Klipspringer <97002039+klipspringr@users.noreply.github.com>
2026-04-12 08:18:33 +0000

    check: logging

--- check/check.sh
+++ check/check.sh
@@ -6,12 +6,12 @@ cd "$(dirname "$0")"
 # shellcheck disable=1091
 . .env/bin/activate
 
-if ! git diff-index --quiet HEAD; then
-    echo "Uncommitted changes in working tree, exiting"
+if [ -n "$(git status --porcelain)" ]; then
+    echo "Uncommitted changes in working tree, exiting" >&2
     exit 1
 fi
 
-git pull 2>&1
+git pull --quiet
 
 python ./check.py
 
@@ -19,7 +19,7 @@ git add ../blog/content/posts
 
 if ! git diff-index --quiet HEAD; then
     git -c "user.name=mefi-activity-automated" -c "user.email=mefi-activity-automated" commit -m "Recent mod actions updated"
-    git push 2>&1
+    git push --quiet
 else
     echo "No changes to blog posts"
 fi
