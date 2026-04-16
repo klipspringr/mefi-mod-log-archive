9131f5a6891fb386662369c3bf97dd6b9a4519b7
Klipspringer <97002039+klipspringr@users.noreply.github.com>
2026-04-16 05:41:38 +0100

    check: introduce locking, and --ff-only

--- check/check.sh
+++ check/check.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 set -euo pipefail
 
+exec 9>"$HOME/.mefi-mod-log-archive.lock"
+flock -n 9 || { echo "Previous run in progress, exiting" >&2; exit 1; }
+
 cd "$(dirname "$0")"
 
 if [ -n "$(git status --porcelain)" ]; then
-    echo "Uncommitted changes in working tree, exiting" >&2
+    echo "Working tree dirty, exiting" >&2
+    git status >&2
     exit 1
 fi
 
-git pull --quiet
+# we don't want to do anything other than fast-forward
+git fetch
+git pull --ff-only --no-progress
 
-$HOME/.local/bin/uv run ./check.py
+"$HOME/.local/bin/uv" run ./check.py
 
-if [ -n "$(git status --porcelain ../blog/content/posts)" ]; then
+status_output=$(git status --porcelain ../blog/content/posts)
+if [ -n "$status_output" ]; then
     git add ../blog/content/posts
     git -c "user.name=mefi-activity-automated" -c "user.email=mefi-activity-automated" commit -m "Recent mod actions updated"
-    git push --quiet
+    git push
 else
     echo "No changes to blog posts"
 fi
