36974fee418bd8fdf7f1a2ef220ff2e8ad1be46a
Klipspringer <97002039+klipspringr@users.noreply.github.com>
2026-05-02 13:56:00 +0100

    upgrade pagefind to 1.5.2

--- blog/serve.sh
+++ blog/serve.sh
@@ -5,5 +5,5 @@ mkdir -p assets/data
 hugo list published > assets/data/published-content.csv
 rm -rf public
 hugo build
-npx -y pagefind@v1.5.0 --site public --glob "posts/**/*.html"
+npx -y pagefind@v1.5.2 --site public --glob "posts/**/*.html"
 hugo server --bind=0.0.0.0
